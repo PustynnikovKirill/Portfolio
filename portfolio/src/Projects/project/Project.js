b483eab82d67d476205637972ce89082593311fc
Pustynnikov Kirill <kiryuha911@yandex.by>
2022-08-01 16:54:01 +0300

    'style'

--- portfolio/src/Projects/project/Project.js
+++ portfolio/src/Projects/project/Project.js
@@ -5,7 +5,7 @@ export const Project = (props) => {
     return (
         <div className={style.projectsBlock}>
             <div className={style.photo} style={props.style}>
-                <a href=''>Смотреть</a>
+                <a className={style.viewBtn} href=''>Смотреть</a>
             </div>
             <div className={style.projectInfo}>
                 <h3 className={style.projectTitle}>{props.title}</h3>
