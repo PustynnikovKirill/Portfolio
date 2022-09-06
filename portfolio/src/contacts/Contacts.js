8b3a2ff235fd19f78da012db5f2fca17d4b9862e
Pustynnikov Kirill <kiryuha911@yandex.by>
2022-09-06 13:28:15 +0300

    'contacts'

--- portfolio/src/contacts/Contacts.js
+++ portfolio/src/contacts/Contacts.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import style from './Contacts.module.scss';
+import {Title} from "../common/components/title/Title";
 
 
 export const Contacts = () => {
     return (
         <div className={style.contactsBlock}>
             <div className={style.contactsContainer}>
-                <h2 className={style.title}>Contacts</h2>
+                <Title text = {"Contacts"} />
+                {/*<h2 className={style.title}>Contacts</h2>*/}
                 <form className={style.contacts}>
-                    <input/>
-                    <input/>
-                    <textarea/>
+                    <input type="text" placeholder= "Name" className={style.inputName}/>
+                    <input type="text" placeholder= "E-mail" className={style.inputEMail}/>
+                    <textarea type="text" placeholder= "Your message"  className={style.textarea}/>
                 </form>
-                <button className={style.button}>send</button>
+                <button className={style.button}>SEND MESSAGE</button>
             </div>
         </div>
     );
