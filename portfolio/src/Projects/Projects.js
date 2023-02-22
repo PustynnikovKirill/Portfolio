c21382854484716209cea4796b1a120abfd924e5
Pustynnikov Kirill <kiryuha911@yandex.by>
2023-02-22 12:42:46 +0300

    'link'

--- portfolio/src/Projects/Projects.js
+++ portfolio/src/Projects/Projects.js
@@ -25,9 +25,9 @@ export const Projects = () => {
                 <div className={style.projectsContainer}>
                     <Title text={"Projects"}/>
                     <div className={style.project}>
-                        <Project style={social} title={'social network'} description={'Communicate with friends, relatives, acquaintances'}/>
-                        <Project style={todolist} title={'todolist'} description={'Write down your tasks, notes.'}/>
-                        <Project style={cards} title={'cards'} description={'Learn, grow.'}/>
+                        <Project  style={social} title={'social network'} description={'Communicate with friends, relatives, acquaintances'} link={'https://PustynnikovKirill.github.io/social-networking'}/>
+                        <Project style={todolist} title={'todolist'} description={'Write down your tasks, notes.'} link={'https://PustynnikovKirill.github.io/Todolist'}/>
+                        <Project style={cards} title={'cards'} description={'Learn, grow.'}link={'https://PustynnikovKirill.github.io/_CARDS'} />
                     </div>
                 </div>
             </Fade>
